0284a4cafb3e36669c053e2fd17a678ff810765a
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2025-02-28 02:36:43 +0900

    фы

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,26 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // resolve: {
+  //   alias: {
+  //     '/api': '/src/api', // Maps /api to src/api
+  //   },
+  // },
   resolve: {
     alias: {
-      '/api': '/src/api', // Maps /api to src/api
+        '@': fileURLToPath(new URL('./src',
+            import.meta.url))
     },
-  },
+},
+server: {
+    host: '0.0.0.0', // Привязка ко всем интерфейсам
+    port: 8080, // Порт (можно изменить)
+    allowedHosts: [
+        '0.0.0.0',
+        'home.thefoxxstuff.net', // Разрешаем твой домен
+        'localhost', // Для локального доступа
+        '192.168.x.x' // Твой локальный IP (замени на актуальный)
+    ]
+},
   })
 
